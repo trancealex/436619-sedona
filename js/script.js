abb9c223bb1be75347b75b6186036741131de7b0
trancealex <33147731+trancealex@users.noreply.github.com>
2017-12-04 15:22:04 +0600

    Финальный проект

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var orderButton = document.querySelector(".order-button");
 var bookingFormHide = document.querySelector(".booking-form-hide");
 
+bookingFormHide.classList.remove("booking-form");
+
 orderButton.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	bookingFormHide.classList.toggle("booking-form");
-}); 
+});

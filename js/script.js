cd48c354c260891466d1b71717eb0ad643f4f3d4
trancealex <33147731+trancealex@users.noreply.github.com>
2017-12-07 01:42:21 +0600

    Исправление ошибок перед второй проверкой

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 var orderButton = document.querySelector(".order-button");
 var bookingFormHide = document.querySelector(".booking-form-hide");
+var bookingForm = document.querySelector(".booking-form");
+var arrival = document.querySelector("[name=arrival]");
+var departure = document.querySelector("[name=departure]");
+var adult = document.querySelector("[name=adult]");
+var child = document.querySelector("[name=child]");
+var search = document.querySelector(".search-button");
 
 bookingFormHide.classList.remove("booking-form");
 
 orderButton.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	bookingFormHide.classList.toggle("booking-form");
+ 	evt.preventDefault();
+ 	bookingFormHide.classList.toggle("booking-form");
+ });
+
+bookingForm.addEventListener ("submit", function (evt) {
+	if (!arrival.value || !departure.value || !adult.value || !child.value) {
+		evt.preventDefault();
+		search.classList.remove("form-error");
+	  	search.offsetWidth = search.offsetWidth;
+	  	search.classList.add("form-error");
+	}
 });

f41461d9544d2a9ff4c0e2b755a1df72e0780032
trancealex <33147731+trancealex@users.noreply.github.com>
2017-12-07 17:04:03 +0600

    Проект к повторной прверке
    Исправлены замечания проверяющего независимого наставника

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 var orderButton = document.querySelector(".order-button");
-var bookingFormHide = document.querySelector(".booking-form-hide");
 var bookingForm = document.querySelector(".booking-form");
 var arrival = document.querySelector("[name=arrival]");
 var departure = document.querySelector("[name=departure]");
 var adult = document.querySelector("[name=adult]");
-var child = document.querySelector("[name=child]");
-var search = document.querySelector(".search-button");
 
-bookingFormHide.classList.remove("booking-form");
+bookingForm.classList.add("booking-form-hide");
 
 orderButton.addEventListener("click", function(evt) {
  	evt.preventDefault();
- 	bookingFormHide.classList.toggle("booking-form");
+ 	bookingForm.classList.toggle("booking-form-hide");
  });
 
-bookingForm.addEventListener ("submit", function (evt) {
-	if (!arrival.value || !departure.value || !adult.value || !child.value) {
-		evt.preventDefault();
-		search.classList.remove("form-error");
-	  	search.offsetWidth = search.offsetWidth;
-	  	search.classList.add("form-error");
-	}
+bookingForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!arrival.value || !departure.value || !adult.value) {
+    bookingForm.classList.remove("booking-form-error");
+    setTimeout(function() {
+    bookingForm.classList.add("booking-form-error");
+    }, 1)
+  } else {
+    bookingForm.submit();
+  }
 });
